51b2e28e412e28546b08513aeb3540753fcd9f53
Hong Jie Tan <hongjtan@gmail.com>
2013-01-09 14:40:30 -0500

    Starting code, unchanged info file.

---
+++ procurement-sum.js
@@ -0,0 +1,6 @@
+// This code allows the user to select numbers in a view and the code will sum it and display it back to the user.
+$(document).ready(function () {
+	$(".views-field").click(function () {
+		console.log("Field Click!");
+	});
+});
