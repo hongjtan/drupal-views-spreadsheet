c866f10ab64eb918f9f258bb94b07334805440f3
Hong Jie Tan <hongjtan@gmail.com>
2013-06-01 15:17:43 -0400

    Added Copyright.

--- views-spreadsheet.js
+++ views-spreadsheet.js
@@ -6,6 +6,8 @@
  *
  * @section     LICENSE
  *
+ * Copyright (C) 2013 Hong Jie Tan 
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
